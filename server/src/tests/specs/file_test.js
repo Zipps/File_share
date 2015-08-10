5e85b185da97bca0999f77aee3e72323bf19b60b
Zipps <shea.granville@gmail.com>
2015-08-10 11:23:45 +1000

    Revert "More tests"
    This reverts commit 073c3575ecbd12f7f3e084567a5514c84d1cd907.

--- server/src/tests/specs/file_test.js
+++ server/src/tests/specs/file_test.js
@@ -29,6 +29,7 @@ describe('Uploads:', function() {
             .get(basePath + '/upload/' + upload._id)
             .expect('Content-Type', /json/)
             .expect(function(res) {
+                console.log(res);
                 var temp = res.body[0];
                 temp.should.have.property('_id');
                 temp.should.not.have.property('key');
@@ -38,7 +39,7 @@ describe('Uploads:', function() {
 });
 
 describe('Files:', function() {
-    it('Should upload the first file to the container', function(done) {
+    it('Should upload the file to the container', function(done) {
         request(app)
             .post(basePath + '/upload/' + upload._id + '/file')
             .attach('test-file', __dirname + '/../data/test_file_one.pdf')
@@ -52,52 +53,6 @@ describe('Files:', function() {
                 file = res.body;
             })
             .expect(201, done);
-    });
-    it('Should upload the second file to the container', function(done) {
-        request(app)
-            .post(basePath + '/upload/' + upload._id + '/file')
-            .attach('test-file', __dirname + '/../data/test_file_two.pdf')
-            .expect('Content-Type', /json/)
-            .expect(function(res) {
-                res.body.should.have.property('key');
-                res.body.should.have.property('filename');
-                res.body.should.have.property('size');
-                res.body.should.have.property('contentType');
-                res.body.should.have.property('uploadDate');
-                file = res.body;
-            })
-            .expect(201, done);
-    });
-    it('Should download a file from the container', function(done) {
-        request(app)
-            .get(basePath + '/upload/' + upload._id + '/file/' + file.key)
-            .expect(function (res) {
-                fs.writeFile(__dirname + '/../output/out_one.pdf', res.body, function(err) {
-                    if (err) throw err;
-                })    ;
-            })
-            .expect(200, done)
-    });
-    it('Should delete a file from the container', function(done) {
-        request(app)
-            .del(basePath + '/upload/' + upload._id + '/file/' + file.key)
-            .expect(204, done)
-    });
-});
-
-describe('Merge:', function() {
-    it('Should merge PDF files together', function(done) {
-        request(app)
-            .post(basePath + '/upload/' + upload._id + '/merge')
-            .expect('Content-Type', /json/)
-            .expect(function(res) {
-                res.body.should.have.property('key');
-                res.body.should.have.property('filename');
-                res.body.should.have.property('size');
-                res.body.should.have.property('contentType');
-                res.body.should.have.property('uploadDate');
-            })
-            .expect(201, done)
     });
 });
 
